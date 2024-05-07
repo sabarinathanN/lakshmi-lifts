74fbdaeb16599eca4b9caf4d01d5b01d8715eb97
sabarinathan N <sabarinathan25052001@gmail.com>
2024-05-08 00:19:05 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="landing-container">
+        <img
+          src="https://img.freepik.com/free-vector/elevator-doors-close-chrome-lift-cabin-entrance_1441-3650.jpg"
+          className="landing-image"
+          alt="Elevator doors"
+        />
+        <div className="landing-content">
+          <h1>LAKHSMI ELEVATORS</h1>
+          <h4>LIFT INSTALLATION AND LIFT MAINTENANCE</h4>
+          <p>We will Available Soon</p>
+        </div>
+      </div>
     </div>
   );
 }
